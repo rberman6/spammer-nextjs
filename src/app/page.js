2ec3e2ed7857e31fc41b949884bbe8a741c9d80c
rberman6 <rittuberman@gmail.com>
2023-12-14 11:33:15 -0700

    fixed cache issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import AllMessages from "../components/AllMessages.jsx";
 
 import PostMessages from "../components/PostMessages.jsx";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
